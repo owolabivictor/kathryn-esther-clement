096bfb8c01bda08150578b081a165ad934dc6d4a
Owolabi Victor <vowolabi16@gmail.com>
2026-07-22 09:18:12 +0100

    Fixed hero height resize problem

--- home.js
+++ home.js
@@ -1,3 +1,13 @@
 // Calculate and assign the hero height
 const hero = document.querySelector(".hero")
-hero.style.height = `${window.innerHeight - nav.offsetHeight}px`
+function setHeroHeight() {
+    const heroHeight = Math.max(
+        window.innerHeight - nav.offsetHeight,
+        500
+    );
+
+    hero.style.height = `${heroHeight}px`;
+}
+
+setHeroHeight()
+window.addEventListener("resize", setHeroHeight)
