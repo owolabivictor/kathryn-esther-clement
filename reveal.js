b09a15f7959ebbf5b2246ee6779e842648f9fb7e
Owolabi Victor <vowolabi16@gmail.com>
2026-07-22 15:20:12 +0100

    Started and finished contact section. Optimized reveal animation across pages

--- reveal.js
+++ reveal.js
@@ -1,8 +1,9 @@
 // General reveal on scroll page animations
 const revealElements = document.querySelectorAll(".reveal");
 const revealLeft = document.querySelectorAll(".reveal-left")
+const revealRight = document.querySelectorAll(".reveal-right")
 
-window.addEventListener("scroll", function () {
+function reveal() {
     revealElements.forEach(function(element) {
         const position = element.getBoundingClientRect();
 
@@ -18,5 +19,16 @@ window.addEventListener("scroll", function () {
             element.classList.add("show");
         }
     });
-    
-});
+
+    revealRight.forEach(function(element){
+        const position = element.getBoundingClientRect()
+
+        if (position.top < window.innerHeight * 0.8){
+            element.classList.add("show")
+        }
+    })
+}
+
+reveal()
+
+window.addEventListener("scroll", reveal);
